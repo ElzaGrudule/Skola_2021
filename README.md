7101c5d95469c21818c969a62b7d905cbb37cdb0
ElzaGrudule <elza.grudule@svg.lv>
2021-09-10 09:06:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-znxiv6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-znxiv6)
---
+++ index.js
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+let poga = document.querySelector('#poga');//atrod elementus lapā
+poga.addEventListener('click', function(){
+  let viesraksts = document.querySelector('#virsraksts');
+  virsraksts.style.color = 'red';
+  //console.log('klikšķis');
+  //alert('Čau!');
+});
+
